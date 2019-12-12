24e855d025146efe88eb3d2cb7a3c8fb3b3eb3fc
Justin A <jarata@gmail.com>
2019-12-12 14:39:56 -0800

    setting up axios call verifying data is being received updating .gitignore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios"
+
+// Components
 import ContentContainer from "./components/content/ContentContainer";
 import Image from "./components/image/Image";
 
 function App() {
+  const [nasaPic, setNasaPic] = useState("");
+  const apiKey = process.env.REACT_APP_API_KEY;
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=" + apiKey)
+      .then(res => {
+        console.log("Response:", res)
+      })
+  }, []);
+  
   return (
     <div className="App">
       App
