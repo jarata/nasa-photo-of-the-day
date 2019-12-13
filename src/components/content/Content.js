2916c4cda283996569188c940c2a0ff4df5a04ec
Justin A <jarata@gmail.com>
2019-12-13 14:39:11 -0800

    built out Content.js passed down required props from ContentContainer.js

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Content = () => {
+const Content = (props) => {
+	console.log("Content:", props)
 	return (
 		<div>
-			Content
+			<p aria-label="title">{props.title}</p>
+			<p aria-label="date">{props.date}</p>
+			<p aria-label="explanation">{props.explanation}</p>
+			<p aria-label="copyright">{props.copyright}</p>
 		</div>
 	)
 }
--- src/components/content/ContentContainer.js
+++ src/components/content/ContentContainer.js
@@ -1,11 +1,16 @@
 import React from "react"
 import Content from "./Content";
 
-const ContentContainer = () => {
+const ContentContainer = (props) => {
+	console.log("ContentContainer:", props)
 	return (
 		<div>
-			ContentContainer
-			<Content/>
+			<Content
+				title={props.nasaData.title}
+				date={props.nasaData.date}
+				explanation={props.nasaData.explanation}
+				copyright={props.nasaData.copyright}
+			/>
 		</div>
 	)
 }
