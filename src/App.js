a6530aae2f95a2b661d5634a2533cbea999e100f
Justin A <jarata@gmail.com>
2019-12-30 19:00:08 -0800

    refactored App.js: added date useState and added to axios added date change to useEffect cleaned up App component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
 import axios from "axios"
 import MediaContainer from "./components/media/MediaContainer";
 import ContentContainer from "./components/content/ContentContainer";
-
-// Components
-
+import DateContainer from "./components/date/DateContainer";
+import { AppDiv } from "./styles/GlobalStyle.styled";
 
 function App() {
   const [nasaData, setNasaData] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
+  const [date, setDate] = useState('');
 
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=" + apiKey)
+      .get("https://api.nasa.gov/planetary/apod?api_key=" + apiKey + "&date=" + date)
       .then(res => {
         console.log("Response:", res);
         setNasaData(res.data);
       })
       .catch(err => console.log("Error:", err))
-  }, []);
+  }, [date]);
   
   return (
-    <div className="App">
-      <div>
-        <MediaContainer mediaData={nasaData}/>
-      </div>
-      <div>
-        <ContentContainer nasaData={nasaData}/>
-      </div>
-    </div>
+    <AppDiv>
+      <DateContainer setDate={setDate}/>
+      <MediaContainer mediaData={nasaData}/>
+      <ContentContainer nasaData={nasaData}/>
+    </AppDiv>
   );
 }
 
