dfe7af6ad75d7c87d5e24a6304a5a8c5890d1c1b
Justin A <jarata@gmail.com>
2019-12-30 19:03:36 -0800

    built date componenets: DateContainer.js, DateInput.js

---
+++ src/components/date/DateInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { InputDiv, Input } from "../../styles/GlobalStyle.styled";
+
+const DateInput = (props) => {
+	// console.log("DateInput:", props);
+	let currDate = new Date().toISOString().split("T")[0];
+	return (
+		<InputDiv>
+			<label htmlFor="date">
+				APOD Archive:
+				<Input type="date" min="1995-06-20" max={currDate} onChange={(event) => props.setDate(event.target.value)}/>
+			</label>
+		</InputDiv>
+	)
+};
+export default DateInput;
